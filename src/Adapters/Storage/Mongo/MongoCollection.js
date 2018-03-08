52a384fab7bd492e406d634ed931a174194f50db
Fernando Mazzon <fer662@gmail.com>
2018-03-08 09:15:17 -0300

    adding more logging making logging not affect performance at all if it's disabled

--- src/Adapters/Storage/Mongo/MongoCollection.js
+++ src/Adapters/Storage/Mongo/MongoCollection.js
@@ -51,20 +51,31 @@ export default class MongoCollection {
       findOperation = findOperation.maxTimeMS(maxTimeMS);
     }
 
-    const date = new Date();
-    return findOperation.toArray().then((results) => {
-      const d = new Date();
-      if (Logger && Logger.PARSE_QUERIES) {
-        Logger.log(Logger.PARSE_QUERIES, this._mongoCollection.s.name + ".rawFind query: " + JSON.stringify(query) + " took " + (d.getTime() - date.getTime()) + "ms");
-      }
-      return results;
-    });
+    const startTime = performance.now();
+    const findOperationToArray = findOperation.toArray();
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return findOperationToArray.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".rawFind query: " + JSON.stringify(query) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return findOperationToArray;
+    }
   }
 
   count(query, { skip, limit, sort, maxTimeMS, readPreference } = {}) {
+    const startTime = performance.now();
     const countOperation = this._mongoCollection.count(query, { skip, limit, sort, maxTimeMS, readPreference });
-
-    return countOperation;
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return countOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".count query: " + JSON.stringify(query) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return countOperation;
+    }
   }
 
   distinct(field, query) {
@@ -76,26 +87,76 @@ export default class MongoCollection {
   }
 
   insertOne(object) {
-    return this._mongoCollection.insertOne(object);
+    const startTime = performance.now();
+    const insertOneOperation = this._mongoCollection.insertOne(object);
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return insertOneOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".insertOne query: " + JSON.stringify(query) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return insertOneOperation;
+    }
   }
 
   // Atomically updates data in the database for a single (first) object that matched the query
   // If there is nothing that matches the query - does insert
   // Postgres Note: `INSERT ... ON CONFLICT UPDATE` that is available since 9.5.
   upsertOne(query, update) {
-    return this._mongoCollection.update(query, update, { upsert: true })
+    const startTime = performance.now();
+    const upsertOneOperation = this._mongoCollection.update(query, update, { upsert: true });
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return upsertOneOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".upsertOne query: " + JSON.stringify(query) + " update " + JSON.stringify(update) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return upsertOneOperation;
+    }
   }
 
   updateOne(query, update) {
-    return this._mongoCollection.updateOne(query, update);
+    const startTime = performance.now();
+    const updateOneOperation = this._mongoCollection.updateOne(query, update);
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return updateOneOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".updateOne query: " + JSON.stringify(query) + " update " + JSON.stringify(update) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return updateOneOperation;
+    }
   }
 
   updateMany(query, update) {
-    return this._mongoCollection.updateMany(query, update);
+    const startTime = performance.now();
+    const updateManyOperation = this._mongoCollection.updateMany(query, update);
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return updateManyOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".updateMany query: " + JSON.stringify(query) + " update " + JSON.stringify(update) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return updateManyOperation;
+    }
   }
 
   deleteMany(query) {
-    return this._mongoCollection.deleteMany(query);
+    const startTime = performance.now();
+    const deleteManyOperation = this._mongoCollection.deleteMany(query);
+    if (Logger && Logger.PARSE_QUERIES && Logger.logEnabled(Logger.PARSE_QUERIES)) {
+      return deleteManyOperation.then((results) => {
+        Logger.log("PARSE_QUERIES", this._mongoCollection.s.name + ".deleteMany query: " + JSON.stringify(query) + " took " + (performance.now() - startTime) + "ms");
+        return results;
+      });
+    }
+    else {
+      return deleteManyOperation;
+    }
   }
 
   _ensureSparseUniqueIndexInBackground(indexRequest) {
